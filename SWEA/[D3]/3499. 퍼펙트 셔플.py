ccdca25f2e4bdc17254abfc486293b7c17a1add5
deokgikim <dukgi77@naver.com>
2020-04-02 00:51:12 +0900

    200402

---
+++ SWEA/[D3]/3499. 퍼펙트 셔플.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for t in range(1, T + 1):
+    n = int(input())
+    card = input().split()
+
+    len_card = (len(card) + 1) // 2
+    a = card[:len_card]
+    b = card[len_card:]
+
+    print('#{}'.format(t), end='')
+    for i in range(len(a)):
+        print(' ',end='')
+        print(a[i], end='')
+        print(' ',end='')
+        if i < len(b):
+            print(b[i], end='')
+    print()
