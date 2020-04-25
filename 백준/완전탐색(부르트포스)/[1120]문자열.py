a3ecff26e43ce650e6e566c4432fcc6c88aa2f93
deokgikim <dukgi77@naver.com>
2020-04-25 17:20:28 +0900

    완전탐색

---
+++ 백준/완전탐색(부르트포스)/[1120]문자열.py
@@ -0,0 +1,13 @@
+a, b = input().split()
+
+min_result = 50
+for i in range(len(b)-len(a)+1):
+    cnt = 0
+    for j in range(len(a)):
+        if a[j] != b[i+j]:
+            cnt += 1
+
+    if cnt < min_result:
+        min_result = cnt
+
+print(min_result)
