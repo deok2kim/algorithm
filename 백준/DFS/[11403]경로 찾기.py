be0bbdfc935a9c533e1dd3d1afb00ec4b1e958e5
deokgikim <dukgi77@naver.com>
2020-04-28 21:26:37 +0900

    DFS/플로이드-워셜

--- 백준/DFS/[11403]경로 찾기.py
+++ 백준/DFS/[11403]경로 찾기.py
@@ -7,6 +7,24 @@ for i in range(n):
             if graph[j][i] == 1 and graph[i][k] == 1:
                 print(i, '->', j, ' -> ', k)
                 graph[j][k] = 1
-
-for i in graph:
-    print(' '.join(list(map(str, i))))
+                
+# bfs 방법
+# for i in graph:
+#     print(' '.join(list(map(str, i))))
+#
+# def bfs(x):
+#     visited = [0] * n
+#     q = [x]
+#     while q:
+#         idx = q.pop(0)
+#         for i in range(n):
+#             if not visited[i]:
+#                 if data[idx][i] == 1:
+#                     visited[i] = 1
+#                     q.append(i)
+#     print(' '.join(map(str, visited)))
+#
+# n = int(input())
+# data = [list(map(int, input().split())) for _ in range(n)]
+# for i in range(n):
+#     bfs(i)
