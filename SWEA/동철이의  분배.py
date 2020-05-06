9ab6b77c2aaf4f8d6ed72213ae33b81b3a4ecf37
deokgikim <dukgi77@naver.com>
2020-05-06 10:44:56 +0900

    ''

--- SWEA/동철이의  분배.py
+++ SWEA/동철이의  분배.py
@@ -1,6 +1,6 @@
 # 시간이 너무 오래걸림;
 def backTracking(idx=0, total=100):
-    global max_num
+    global min_sum
     global visit
     if total <= max_num:
         return
@@ -24,7 +24,7 @@ for t in range(1, int(input()) + 1):
     # 람다 쓰는 방법
     # field = [list(map(lambda x: int(x) / 100, input().split())) for _ in range(N)]
     visit = [False] * N
-    max_num = 0
+    min_sum = 0
     # for i in field:
     #     print(i)
     # print()
@@ -32,4 +32,4 @@ for t in range(1, int(input()) + 1):
     # print(max_num)
     # max_num = max_num * (0.1**(N*2))
     print('#{}'.format(t), end=' ')
-    print(format(max_num, '0.6f'))
+    print(format(min_sum, '0.6f'))
--- 백준/다이나믹프로그래밍/[1912]연속합.py
+++ 백준/다이나믹프로그래밍/[1912]연속합.py
@@ -6,11 +6,11 @@ numbers = list(map(int, sys.stdin.readline().split()))
 # print(numbers)
 
 result = -1001
-max_sum = [0] * (n)
+min_sum = [0] * (n)
 
 for i in range(n):
-    max_sum[i] = max(numbers[i] + max_sum[i - 1], numbers[i])
-    result = max(result, max_sum[i])
+    min_sum[i] = max(numbers[i] + min_sum[i - 1], numbers[i])
+    result = max(result, min_sum[i])
     # print(result, max_sum)
 
 print(result)
