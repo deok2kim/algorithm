9f57129fde80da7063f4855c87e0753472f3dd1f
deok2kim <dukgi77@naver.com>
2020-07-30 22:42:16 +0900

    array[array.length - 1]

--- JavaScript/영어 끝말잇기.js
+++ JavaScript/영어 끝말잇기.js
@@ -1,19 +1,20 @@
 function solution(n, words) {
     var answer = [0, 0];
+    // 이미 사용한 단어를 넣을 배열
     var usedWords = []
-    // 한 글자일 경우 시작 할 수 없다.
-    if (words[0].length == 1) {
-        return [1, 1]
-    } else {
-        usedWords.push(words[0])
-    }
+    // 1번 플레이어부터 시작
+    usedWords.push(words[0])
     for (let i = 1; i < words.length; i++) {
         var beforeWord = words[i-1]
         var currentWord = words[i]
+        // 이전단어의 맨 뒷글자와 현재단어의 맨 앞글자가 같고
+        // 이전에 사용한적이 없는 단어라면
         if (beforeWord[beforeWord.length - 1] == currentWord[0]
             && usedWords.indexOf(currentWord) == -1) {
+            // 계속 게임을 진행
             usedWords.push(currentWord)
         } else {
+            // 그게 아니라면 몇번째 턴인지, 몇번째 플레이어 인지 계산해서 리턴해준다.
             var turn = parseInt(i / n) + 1
             var player = i % n + 1
             return answer = [player, turn]
