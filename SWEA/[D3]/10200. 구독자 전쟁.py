4f079411a380e9922f88ff15448c44b59272a318
deokgikim <dukgi77@naver.com>
2020-08-05 17:06:22 +0900

    D3

---
+++ SWEA/[D3]/10200. 구독자 전쟁.py
@@ -0,0 +1,10 @@
+for tc in range(1, 1 + int(input())):
+    n, a, b = map(int, input().split())
+
+    max_answer = min(a, b)
+    print(a, b)
+    min_answer = min(a, b) - (n - max(a, b))
+    if min_answer < 0:
+        min_answer = 0
+
+    print('#{} {} {}'.format(n, max_answer, min_answer))
