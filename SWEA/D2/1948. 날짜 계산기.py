08581c8b491f1366531cb995c3826908c2cfbc68
deok2kim <dukgi77@naver.com>
2020-08-23 16:02:35 +0900

    update algorithm SWEA D2

--- SWEA/D2/1948. 날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기.py
@@ -1,8 +1,18 @@
+# T = int(input())
+# for t in range(1, T+1):
+#     sm, sd, em, ed = map(int, input().split())
+#     month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     result = 1 + ed - sd
+#     for i in range(sm, em):
+#         result += month[i]
+#     print('#{} {}'.format(t, result))
+
+
+# datetime 사용법
+import datetime
 T = int(input())
-for t in range(1, T+1):
-    sm, sd, em, ed = map(int, input().split())
-    month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    result = 1 + ed - sd
-    for i in range(sm, em):
-        result += month[i]
-    print('#{} {}'.format(t, result))
+for tc in range(1, T + 1):
+    a1, a2, b1, b2 = map(int, input().split())
+    a = datetime.datetime(2019,b1,b2) - datetime.datetime(2019,a1,a2)
+    b = str(a).split(' ')[0]
+    print('#{} {}'.format(tc, int(b)+1))
