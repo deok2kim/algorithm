55b4653793969018285f515b4a70ccd85ba217e1
deokgikim <dukgi77@naver.com>
2020-11-09 11:08:44 +0900

    Add algorithm 1109 G2 우선순위 큐

--- algo_study/1109/2014. 소수의 곱.py
+++ algo_study/1109/2014. 소수의 곱.py
@@ -1,22 +1,20 @@
+import heapq
+
 if __name__ == '__main__':
     K, N = map(int, input().split())
-    numbers = list(map(int, input().split()))
-    answer = []
-    max_num = 1
-    for num in numbers:
-        max_num *= num
+    prime = list(map(int, input().split()))
 
-    dp = [0] * (max_num + 1)
-    dp[1] = 1
-    for i in range(2, len(dp)):
-        for num in numbers:
+    pq = []  # 곱한 값이 들어갈 리스트 (우선순위 큐)
+    for num in prime:
+        heapq.heappush(pq, num)
 
-            if i / num == i // num and dp[i // num] == 1:
-                dp[i] = 1
+    for i in range(N):  # 큐에서 N번 빼면 그 값이 N번째 값이다. (우선순위 큐)
+        num = heapq.heappop(pq)
+        for j in range(K):  # 뺸 값에 소수를 곱해서 새로운 값을 만든다.
+            new_num = num * prime[j]
+            heapq.heappush(pq, new_num)
 
-    cnt = 0
-    for i in range(numbers[0], len(dp)):
-        if dp[i] == 1:
-            cnt += 1
-            if cnt == N:
-                print(i)
+            if num % prime[j] == 0:  # 2 X 3은 되지만 3 X 2는 안되는 느낌으로 중복제거
+                break
+    else:
+        print(num)
