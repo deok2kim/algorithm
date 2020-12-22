40f26eb601ed2055026cd9f77077b743f9599010
deok2kim <dukgi77@naver.com>
2020-12-22 17:14:23 +0900

    delete files

--- JavaScript/가장 큰 수.js
+++
@@ -1,9 +0,0 @@
-function solution(numbers) {
-    var answer = '';
-    numbers.sort((a, b) => {
-        return (b + '' + a) * 1 - (a + '' + b) * 1
-    })
-    // console.log(numbers)
-    answer = numbers.join('')
-    return answer[0] == '0' ? '0' : answer;
-}
